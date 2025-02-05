76916991cc92bf90d9f0215a787a08204da4210c
zakariadjendli <202157206@cegepsherbrooke.qc.ca>
2025-02-05 14:24:22 -0500

    Delete readme.py

--- readme.py
+++
@@ -1,3 +0,0 @@
-le but est de remplir un formulaire pour un contrat de travail,en vous posant des questions sur vous et votres experience dans le marché du travail
-l'executer en cliquant sur run
-la plupart des entrées sont oui ou non et les sorties dependeront de vos entrées, si vous ne repondez pas correctement le programme vous le dira simplement
